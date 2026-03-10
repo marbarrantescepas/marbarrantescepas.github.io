36e97de9c73638963ce82ef4fa164d5a947746f0
Mar Barrantes Cepas <mar.bc3@gmail.com>
2026-03-10 23:36:57 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -26,8 +26,8 @@ export const template = {
 	website_url: 'https://marbarrantescepas.github.io', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
-	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'garden', // Select one of the Daisy UI Themes or create your own
+	darkTheme: 'forest', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
     base: '' // Repository name starting with /
